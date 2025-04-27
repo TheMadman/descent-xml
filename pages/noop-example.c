50a9281f12fff362b90156093a11365a197ba998
Marcus Harrison <marcus@harrisonland.co.uk>
2025-04-27 20:58:23 +0200

    Add first tutorial
    I would've done more but I uncovered a bug
    by accident that I fixed in the previous commit

---
+++ pages/noop-example.c
@@ -0,0 +1,36 @@
+#include <libadt/str.h>
+#include <xmltree/parse.h>
+
+typedef struct xmltree_lex lex_t;
+
+#define str libadt_str_literal
+
+#define lex xmltree_lex_init
+#define eof xmltree_classifier_eof
+#define unexpected xmltree_classifier_unexpected
+#define error xmltree_parse_error
+#define parse xmltree_parse_cstr
+
+int is_end_type(lex_t token)
+{
+	return token.type == eof;
+}
+
+int is_error_type(lex_t token)
+{
+	return token.type == unexpected
+		|| token.type == error;
+}
+
+int main()
+{
+	lex_t token = lex(str("<element>Hello, world!</element>"));
+	while (!is_end_type(token)) {
+		if (is_error_type(token)) {
+			// Handle error
+			return 1;
+		}
+
+		token = parse(token, NULL, NULL, NULL);
+	}
+}
---
+++ pages/tutorial-01.md
@@ -0,0 +1,15 @@
+\page tutorial-01 Tutorial 1
+
+\section Hello, World
+
+This program will be the basis for future tutorials.
+
+It will:
+
+- initialize a token from an XML string
+- iterate the token from the beginning of the XML to the end
+- exit the program
+
+\include{lineno} noop-example.c
+
+
