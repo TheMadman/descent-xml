a20dddd348fe294b7be078948201620cd02c0e76
Marcus Harrison <marcus@harrisonland.co.uk>
2025-06-08 18:05:25 +0200

    Move default initialization of dependency to deafult.nix

--- build.nix
+++ build.nix
@@ -1,13 +1,7 @@
 {
-	fetchFromGitHub,
 	stdenv,
 	cmake,
-	libadt ? import (fetchFromGitHub {
-		owner = "TheMadman";
-		repo = "libadt";
-		rev = "6ab226d4f0b5fb91d00d81810b60e82a88f1fd99";
-		hash = "sha256-87YhgCmCBvJaZ2w+I1BKILUOSuF8/Isip3FWAPDYZIQ=";
-	}),
+	libadt,
 }:
 
 stdenv.mkDerivation {
--- default.nix
+++ default.nix
@@ -1,4 +1,12 @@
 let
+	libadt = if pkgs ? libadt
+		then pkgs.libadt
+		else import (pkgs.fetchFromGitHub {
+			owner = "TheMadman";
+			repo = "libadt";
+			rev = "6ab226d4f0b5fb91d00d81810b60e82a88f1fd99";
+			hash = "sha256-87YhgCmCBvJaZ2w+I1BKILUOSuF8/Isip3FWAPDYZIQ=";
+		});
 	pkgs = import <nixpkgs> {};
 in
-pkgs.callPackage ./build.nix {}
+pkgs.callPackage ./build.nix { inherit libadt; }
